5b470e64fc069ca478c63f74af2eaae2015d9908
JakubSmo <127049024+JakubSmo@users.noreply.github.com>
2023-10-31 19:33:20 +0100

    formating

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,41 +1,29 @@
-// Pobranie przycisków
 const startButton = document.querySelector('[data-start]');
 const stopButton = document.querySelector('[data-stop]');
 
-// Funkcja do generowania losowego koloru
-function getRandomHexColor() {  
-    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-// Deklaracja zmiennej przechowującej identyfikator interwału
 let intervalId;
 
-// Funkcja rozpoczynająca zmianę koloru tła
 function startColorChanging() {
-    // Wyłączenie przycisku "Start"
-    startButton.disabled = true;
+  startButton.disabled = true;
 
-    // Włączenie przycisku "Stop"
-    stopButton.disabled = false;
+  stopButton.disabled = false;
 
-    // Uruchomienie interwału zmieniającego kolor
-    intervalId = setInterval(function() {
-        document.body.style.backgroundColor = getRandomHexColor();
-    }, 1000);
+  intervalId = setInterval(function () {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
 }
 
-// Funkcja zatrzymująca zmianę koloru tła
 function stopColorChanging() {
-    // Włączenie przycisku "Start"
-    startButton.disabled = false;
+  startButton.disabled = false;
 
-    // Wyłączenie przycisku "Stop"
-    stopButton.disabled = true;
+  stopButton.disabled = true;
 
-    // Zatrzymanie interwału zmieniającego kolor
-    clearInterval(intervalId);
+  clearInterval(intervalId);
 }
 
-// Dodanie event listenerów do przycisków
 startButton.addEventListener('click', startColorChanging);
 stopButton.addEventListener('click', stopColorChanging);
